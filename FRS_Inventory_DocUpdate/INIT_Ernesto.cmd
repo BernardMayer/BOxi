0c040060d731d5b715ac5be4abb9c0753b1bdfaf
Bernard Mayer <Marcel_Ducamion@yahoo.fr>
2017-11-13 20:33:52 +0100

    MaJ 20171113

---
+++ FRS_Inventory_DocUpdate/INIT_Ernesto.cmd
@@ -0,0 +1,14 @@
+@cls
+@setlocal enabledelayedexpansion
+@rem 65001 UTF8, 1252 	West European Latin, 850 	Multilingual (Latin I), 437 	United States
+@rem cscript.exe //U
+@rem @CHCP 1252
+
+@set homeDir=".\"
+@set sqlBin="C:\CAT_dskD\myTools\sqlite\sqlite3.exe"
+@set sqlHome="C:\RepoGit\BOxi\FRS_Inventory_DocUpdate"
+@set sqlDB="%sqlHome%\BO_inventories.sqlite"
+
+@echo Initialisation de la DB
+@set sqlFile=%homeDir%\DocUpdate_InitializeDB.sql
+%sqlBin% %sqlDB% < %sqlFile%

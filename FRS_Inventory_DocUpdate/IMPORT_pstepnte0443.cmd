f119ce9f41aa1f76251cdc0fd8de7110649fd5a6
Bernard Mayer <25210782+BernardMayer@users.noreply.github.com>
2017-11-15 15:52:41 +0100

    MaJ 20171115

--- FRS_Inventory_DocUpdate/IMPORT_pstepnte0443.cmd
+++ FRS_Inventory_DocUpdate/IMPORT_pstepnte0443.cmd
@@ -14,20 +14,29 @@
 @set dataInPattern="Liste_Docs_*.csv"
 @set importScript=import.py
 
+@REM Compilation des fichiers inventaires des FRS en 1 seul
+@set datasInDir="C:\temp\Data-FullClientOnly-ToutesCaisses"
+@set datasInPattern="Liste_Docs_*.csv"
+@set tmpDir=%datasInDir%\"compil"
+@set tmpFile="FRS_Inventory_DocUpdate.tmp"
+if not exist %tmpDir% mkdir %tmpDir%
+copy %datasInDir%\%datasInPattern% %tmpDir%\%tmpFile%
+
 @echo Alimentation de la DB
 set BO_IMPORT_INVENTORY_RUN_ID=7
 %pyBin% %importScript% "D:\BOXI\Liste_Docs_TtesCrs\Data-FullClientOnly-ToutesCaisses\Liste_Docs_CL_Documents communautaires.csv" %sqlDB%
 goto EOF
 
-
+pause
+del %tmpDir%\%tmpFile%
 
 
 @REM -- Liste des fichiers
-echo. > %dataInHome%\test.txt
+@rem echo. > %dataInHome%\test.txt
 @REM REM REM FOR /f "delims=" %%c IN ('type "BO_ListeDesPlateformes.txt"') DO C:\Windows\SysWOW64\cscript.exe //Nologo //T:3600 .\%Nom_script%.vbs %username% %pwd% %%c %FRS_sommet% >> %FICH_LOG% 2>&1
 @rem FOR /f "delims=" %%c IN ('dir /b "%dataInHome%\%dataInPattern%"') DO @echo %%c >> "%dataInHome%\test.txt"
 @rem FOR /f "delims=" %%c IN ('dir /b %dataInHome%\%dataInPattern%') DO @echo %%c
-FOR /f "delims=" %%f IN ('dir /b %dataInHome%\%dataInPattern%') DO %pyBin% %importScript% %%f %sqlDB% >> "%dataInHome%\test.txt"
+@rem FOR /f "delims=" %%f IN ('dir /b %dataInHome%\%dataInPattern%') DO %pyBin% %importScript% %%f %sqlDB% >> "%dataInHome%\test.txt"
 
 @rem @set sqlFile=%homeDir%\DocUpdate_InitializeDB.sql
 @rem %sqlBin% %sqlDB% < %sqlFile%
